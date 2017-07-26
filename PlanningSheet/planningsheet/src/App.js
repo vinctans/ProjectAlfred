71864dfc619c4dd1bf387a4ca285c3a894eb429b
vinctans <vinctans@users.noreply.github.com>
2017-07-26 11:08:18 +0800

    Fix App.js

--- PlanningSheet/planningsheet/src/App.js
+++ PlanningSheet/planningsheet/src/App.js
@@ -12,8 +12,6 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-		  <br>
-		  Wendy was here.
         </p>
       </div>
     );
